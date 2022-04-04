f917bee57d6d15914b2de3d6ad5c52726234fc92
Amet-Vikram <amet1197@gmail.com>
2022-04-04 10:04:10 +0530

    Allow Player to choose between X and O

--- src/com/bridgelabz/tictactoe/Main.java
+++ src/com/bridgelabz/tictactoe/Main.java
@@ -1,12 +1,22 @@
 package com.bridgelabz.tictactoe;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
+    void playerChance(int turn){
+        //will run inside check_winner method
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Row: ");
+        Integer row = scan.nextInt();
+        System.out.println("Enter Column: ");
+        Integer col = scan.nextInt();
+    }
 
     public static void main(String[] args) {
         TicTacToeGame game = new TicTacToeGame();
         System.out.println(Arrays.deepToString(game.getBoard()));
+//        game.playerChoice();
     }
 }
--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -1,25 +1,66 @@
 package com.bridgelabz.tictactoe;
 
+import java.util.Hashtable;
+import java.util.Scanner;
 
 public class TicTacToeGame {
-    static int[][] board = new int[3][3];
+    static char[][] board = new char[3][3];
+    Hashtable<String, String> playerSymbol = new Hashtable<>();
     Integer position;
 
     TicTacToeGame(){
         populate();
     }
+
     static void populate(){
-        int num = 1;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                board[i][j] = num;
-                num++;
+                board[i][j] = '-';
             }
         }
     }
 
-    public int[][] getBoard() {
+    void drawBoard(char[][] board) {
+        System.out.println("Board:");
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                System.out.print(aChar);
+            }
+            System.out.println();
+        }
+    }
+
+    public char[][] getBoard() {
         return board;
     }
+
+    public void playerChoice(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Let's play Tic Tac Toe!");
+        System.out.print("Player 1, what is your name? ");
+        String p1 = scan.nextLine();
+        System.out.print("Player 2, what is your name? ");
+        String p2 = scan.nextLine();
+
+        System.out.println("Player 1 - X or O?");
+        String choice = scan.next();
+        if(choice.equals("X")){
+            playerSymbol.put(p1, "X");
+            playerSymbol.put(p2, "O");
+        }
+        else{
+            playerSymbol.put(p1, "O");
+            playerSymbol.put(p2, "X");
+        }
+    }
+
+    void checkWinner(){
+        boolean gameEnded = false;
+        while (!gameEnded){
+
+        }
+    }
+
+
 
 }
